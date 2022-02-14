1cf625a430288bb4ed14388c042aa10dc9bdb0e4
Thomas Felix <tfelix@meshcloud.io>
2022-02-14 21:08:42 +0100

    wip

--- shop/src/app/app.component.ts
+++ shop/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import LitJsSdk from 'lit-js-sdk';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  private readonly client = new LitJsSdk.LitNodeClient();
+
+  ngOnInit(): void {
+    from(this.client.connect()).subscribe(x => console.log('Lit Loaded'));
+  }
 
 }
