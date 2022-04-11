1eb12c864d5b77e7e09b95f790ea1708ef0ae115
Thomas Felix <tfelix@meshcloud.io>
2022-04-11 23:39:33 +0200

    Initial preparations for dev deployment

---
+++ website/src/environments/environment.dev.ts
@@ -0,0 +1,9 @@
+import { Environment } from "./environment-model";
+
+export const environment: Environment = {
+  defaultShopName: 'w3shop',
+  production: true,
+  injectMocks: false,
+  shopFactoryAddr: '0xd11bfA9bBe0F43960FEc4aE5677aE09Cb4059F7a',
+  ownerNftArweaveId: 'V3wuc162d70sqxH55qaL8xFMWvDhASGPy7c4XVA3Vqg'
+};
