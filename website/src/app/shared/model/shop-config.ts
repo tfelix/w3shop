2c48d224b8736b23a285dad28d8b8e5a26279183
Thomas Felix <tfelix@meshcloud.io>
2022-11-07 22:02:50 +0100

    Adds comments

--- website/src/app/shared/model/shop-config.ts
+++ website/src/app/shared/model/shop-config.ts
@@ -1,17 +1,41 @@
 import { URI } from "./url";
 
 export type ShopConfigVersion = '1';
-export type ShopItemList = {[key: string]: URI};
+export type ShopItemList = { [key: string]: URI };
 
 export interface ShopConfig {
   version: ShopConfigVersion;
 }
 
 export interface ShopConfigV1 extends ShopConfig {
+  /**
+   * Name of the shop
+   */
   shopName: string;
-  currency: string | 'ETH';
+
+  /**
+   * Currency token address the shop wants to receive. 0x0 means the shop
+   * accepts ETH.
+   */
+  currency: string;
+
+  /**
+   * A short description of the shop often used as summary or tag-line.
+   */
   shortDescription: string;
+
+  /**
+   * Longer descrpition of the shop can be formatted as Markdown.
+   */
   description: string;
+
+  /**
+   * A list of keywords that describe the contents of your shop.
+   */
   keywords: string[];
+
+  /**
+   * The list of the items this shop sells.
+   */
   items: ShopItemList;
 }
