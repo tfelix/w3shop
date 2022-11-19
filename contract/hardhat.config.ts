f42b7b0e2195b5e03ed179db4a08d4248fc134ce
Thomas Felix <tfelix@meshcloud.io>
2022-11-19 16:44:25 +0100

    Upgrade to the dependencies

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -18,16 +18,13 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: '0.8.9',
+        version: '0.8.17',
         settings: {
           optimizer: {
             enabled: true
           }
         }
-      },
-      {
-        version: '0.7.6',
-      },
+      }
     ],
   },
   namedAccounts: {
