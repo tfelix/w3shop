57c21c35fcf6c5dc76a6b21cb027e1af83a30acd
Thomas Felix <tfelix@meshcloud.io>
2022-12-02 00:55:35 +0100

    Fixes Angular tests Makes W3PaymentProcessor non-reentrant

--- website/src/app/core/shop/shop-identifier.service.spec.ts
+++ website/src/app/core/shop/shop-identifier.service.spec.ts
@@ -1,23 +1,25 @@
 import { TestBed } from '@angular/core/testing';
 import { expect } from 'chai';
+import { add } from 'cypress/types/lodash';
 
 import { ShopIdentifierService } from './shop-identifier.service';
 
 fdescribe('ShopIdentifierService', () => {
-  let service: ShopIdentifierService;
+  let sut: ShopIdentifierService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ShopIdentifierService);
+    sut = TestBed.inject(ShopIdentifierService);
   });
 
   it('generates a valid smart contract identifier', () => {
     const addr = '0xeea25755bb5680838c7df5ab049be3acd968eecf';
-    const identifier = service.buildSmartContractIdentifier(addr);
-    service.setIdentifier(identifier);
+    const identifier = sut.buildSmartContractIdentifier(addr);
 
-    service.smartContractDetails$.subscribe(sd => {
-      expect(sd.contractAddress).to.eq(addr);
-    });
+    const details = sut.getSmartContractDetails(identifier);
+
+    expect(details.contractAddress).to.equal(add);
+    expect(details.chainId).to.equal(42161);
+    expect(details.identifier).to.equal(identifier);
   });
 });
